8d2d0fe0da95110df9c505f7f0503955e2c94871
Jose R. Zapata <jjrzg@hotmail.com>
2024-03-29 23:32:27 -0500

    🔨 add etl script

--- src/libs/data_etl/data_etl.py
+++ src/libs/data_etl/data_etl.py
@@ -1,4 +1,23 @@
+"""Functions for data extraction, transformation, and loading (ETL)
+
+Author: Jose R. Zapata <https://joserzapata.github.io/>
+"""
+
 import pandas as pd
 
-url_data = "https://www.openml.org/data/get_csv/16826755/phpMYEkMl"
-titanic_df = pd.read_csv(url_data, low_memory=False)  # no parsing of mixed types
+
+def extract_data(url: str, use_columns: list, raw: str) -> None:
+    """Extract csv data from a given URL and save it as a parquet file.
+    use_columns is a list of column names to use from the data.
+
+    Args:
+        url (str):          URL from which to extract the data.
+        use_columns (list): List of column names to use from the data.
+        raw (str):          Path to save the raw data.
+
+    Returns:
+        None
+
+    """
+    df_raw = pd.read_csv(url, usecols=use_columns, low_memory=False)
+    df_raw.to_parquet(raw, engine="pyarrow")  # no parsing of mixed
