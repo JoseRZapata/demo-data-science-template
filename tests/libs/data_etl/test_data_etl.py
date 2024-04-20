71af08466a3d1df160a3ea4b915ebab9ec1e55e4
Jose R. Zapata <jjrzg@hotmail.com>
2024-04-20 10:21:38 -0500

    ✅ add data type conversion test

--- tests/libs/data_etl/test_data_etl.py
+++ tests/libs/data_etl/test_data_etl.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pytest
-from libs.data_etl.data_etl import download_csv_url
+from libs.data_etl.data_etl import data_type_conversion, download_csv_url
 from pytest_mock import MockerFixture
 
 
@@ -31,3 +31,32 @@ def test_download_csv_url(mock_read_csv: MockerFixture) -> None:
     df_raw = pd.read_csv(raw_path, usecols=use_columns)
     assert df_raw.shape == (3, 2)
     assert df_raw.columns.tolist() == use_columns
+
+
+def test_data_type_conversion() -> None:
+    # Crear un DataFrame de prueba
+    data = pd.DataFrame(
+        {
+            "cat_column": ["a", "b", "c"],
+            "float_column": [1.1, 2.2, 3.3],
+            "int_column": [1, 2, 3],
+            "bool_column": [True, False, True],
+        }
+    )
+
+    # Definir las columnas para cada tipo de datos
+    cat_columns = ["cat_column"]
+    float_columns = ["float_column"]
+    int_columns = ["int_column"]
+    bool_columns = ["bool_column"]
+
+    # Llamar a la función data_type_conversion
+    converted_data = data_type_conversion(
+        data, cat_columns, float_columns, int_columns, bool_columns
+    )
+
+    # Verificar que los tipos de datos de las columnas son correctos
+    assert converted_data["cat_column"].dtype.name == "category"
+    assert converted_data["float_column"].dtype.name == "float64"
+    assert converted_data["int_column"].dtype.name == "int64"
+    assert converted_data["bool_column"].dtype.name == "bool"
